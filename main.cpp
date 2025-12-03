576bd442e3109d3520eeba9f1f232d8d8405a57d
Костян <varenik.kotyarka@gmail.com>
2025-12-04 02:22:59 +0300

    made good main

--- main.cpp
+++ main.cpp
@@ -8,74 +8,89 @@
 #include <limits>
 
 using namespace std::chrono_literals;
+std::mutex print_mutex;
 
-const int MAX_X = 500;
-const int MAX_Y = 500;
+struct print : std::stringstream
+{
+    ~print()
+    {
+        static std::mutex mtx;
+        std::lock_guard<std::mutex> lck(print_mutex);
+        std::cout << this->str();
+        std::cout.flush();
+    }
+};
 
-class CombatManager
+struct FightEvent
+{
+    std::shared_ptr<NPC> attacker;
+    std::shared_ptr<NPC> defender;
+};
+
+class FightManager
 {
 private:
-    std::queue<std::pair<std::shared_ptr<NPC>, std::shared_ptr<NPC>>> events;
+    std::queue<FightEvent> events;
+    FightManager() {}
     std::mutex mtx;
-    bool combat_mode = false;
     
-    CombatManager() = default;
-
 public:
-    static CombatManager& get()
+    static FightManager &get()
     {
-        static CombatManager instance;
+        static FightManager instance;
         return instance;
     }
-    
-    void set_combat_mode(bool mode) { combat_mode = mode; }
-    bool is_combat_mode() const { return combat_mode; }
-    
-    void add_combat_event(std::shared_ptr<NPC> attacker, std::shared_ptr<NPC> defender)
+
+    void add_event(FightEvent &&event)
     {
-        if (!combat_mode) return;
-        
         std::lock_guard<std::mutex> lck(mtx);
-        events.push({attacker, defender});
+        events.push(event);
     }
-    
-    void process_events(set_t& npcs)
+
+    void clear_events()
     {
         std::lock_guard<std::mutex> lck(mtx);
-        
-        while (!events.empty())
-        {
-            auto [attacker, defender] = events.front();
+        while (!events.empty()) {
             events.pop();
-            
-            if (!attacker->is_alive() || !defender->is_alive())
-                continue;
-            
-            bool attacker_wins = defender->accept(attacker);
-            bool defender_wins = attacker->accept(defender);
-            
-            if (attacker_wins && defender_wins)
-            {
-                attacker->must_die();
-                defender->must_die();
-            }
-            else if (attacker_wins)
-            {
-                defender->must_die();
-            }
-            else if (defender_wins)
-            {
-                attacker->must_die();
-            }
         }
-        
-        set_t alive_npcs;
-        for (auto& npc : npcs)
+    }
+
+    void operator()()
+    {
+        while (true)
         {
-            if (npc->is_alive())
-                alive_npcs.insert(npc);
+            std::optional<FightEvent> event;
+            if (!events.empty())
+            {
+                std::lock_guard<std::mutex> lck(mtx);
+                event = events.front();
+                events.pop();
+            }
+
+            if (event)
+                if (event->attacker->is_alive())
+                    if (event->defender->is_alive())
+                    {
+                        bool attacker_wins = event->defender->accept(event->attacker);
+                        bool defender_wins = event->attacker->accept(event->defender);
+                        
+                        if (attacker_wins && defender_wins)
+                        {
+                            event->attacker->must_die();
+                            event->defender->must_die();
+                        }
+                        else if (attacker_wins)
+                        {
+                            event->defender->must_die();
+                        }
+                        else if (defender_wins)
+                        {
+                            event->attacker->must_die();
+                        }
+                    }
+            
+            std::this_thread::sleep_for(100ms);
         }
-        npcs = std::move(alive_npcs);
     }
 };
 
@@ -101,15 +116,15 @@ void add_npc_manual(set_t& npcs)
         return;
     }
     
-    std::cout << "X (0-" << MAX_X << "): ";
+    std::cout << "X (0-500): ";
     int x;
     std::cin >> x;
     
-    std::cout << "Y (0-" << MAX_Y << "): ";
+    std::cout << "Y (0-500): ";
     int y;
     std::cin >> y;
     
-    if (x < 0 || x > MAX_X || y < 0 || y > MAX_Y)
+    if (x < 0 || x > 500 || y < 0 || y > 500)
     {
         clear_input();
         return;
@@ -157,66 +172,169 @@ void start_combat_mode(set_t& npcs)
     std::cout << "\n=== БОЕВОЙ РЕЖИМ ===" << std::endl;
     
     int distance;
-    std::cout << "Дальность: ";
+    std::cout << "Дальность боя: ";
     std::cin >> distance;
     
     if (distance <= 0)
     {
+        std::cout << "Дальность должна быть положительной!" << std::endl;
         clear_input();
         return;
     }
     
-    CombatManager::get().set_combat_mode(true);
-    
-    std::cout << "Боевой режим запущен!" << std::endl;
     clear_input();
-    std::cin.get();
     
-    CombatManager::get().set_combat_mode(false);
+    const int MAX_X = 500;
+    const int MAX_Y = 500;
+    const int DISTANCE = distance;
     
-    CombatManager::get().process_events(npcs);
-}
-
-void collision_checker_thread(set_t& npcs, int distance)
-{
-    while (true)
+    FightManager::get().clear_events();
+    
+    std::cout << "Боевой режим запущен! Нажмите Enter для остановки..." << std::endl;
+    
+    std::thread fight_thread(std::ref(FightManager::get()));
+    
+    bool combat_running = true;
+    
+    std::thread move_thread([&npcs, MAX_X, MAX_Y, DISTANCE, &combat_running]()
     {
-        if (!CombatManager::get().is_combat_mode())
+        while (combat_running)
         {
-            std::this_thread::sleep_for(100ms);
-            continue;
-        }
-        
-        for (const auto& npc1 : npcs)
-        {
-            if (!npc1->is_alive()) continue;
+            for (const std::shared_ptr<NPC> & npc : npcs)
+                if(npc->is_alive())
+                    npc->move(std::rand() % 40 - 20, 
+                              std::rand() % 40 - 20, MAX_X, MAX_Y);
+
+            for (const std::shared_ptr<NPC> & npc : npcs)
+                for (const std::shared_ptr<NPC> & other : npcs) 
+                    if ((other != npc) && 
+                        (npc->is_alive()) && 
+                        (other->is_alive()) && 
+                        (npc->is_close(other, DISTANCE)))
+                        FightManager::get().add_event({npc, other});
             
-            for (const auto& npc2 : npcs)
+            std::this_thread::sleep_for(10ms);
+        }
+    });
+    
+    const int grid = 20;
+    const int step_x = MAX_X / grid;
+    const int step_y = MAX_Y / grid;
+    
+    std::thread input_thread([&combat_running]() {
+        std::cin.get();
+        combat_running = false;
+    });
+    
+    while (combat_running)
+    {
+        std::array<char, grid * grid> fields{0};
+        
+        for (const std::shared_ptr<NPC> &npc : npcs)
+        {
+            const auto [x, y] = npc->position();
+            int i = x / step_x;
+            int j = y / step_y;
+            
+            if (i >= 0 && i < grid && j >= 0 && j < grid)
             {
-                if (!npc2->is_alive()) continue;
-                if (npc1 == npc2) continue;
-                
-                if (npc1->is_close(npc2, distance))
+                if (npc->is_alive())
                 {
-                    CombatManager::get().add_combat_event(npc1, npc2);
+                    switch (npc->get_type())
+                    {
+                    case DragonType:
+                        fields[i + grid * j] = 'D';
+                        break;
+                    case KnightType:
+                        fields[i + grid * j] = 'K';
+                        break;
+                    case ElfType:
+                        fields[i + grid * j] = 'E';
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                else
+                {
+                    fields[i + grid * j] = '.';
                 }
             }
         }
         
-        std::this_thread::sleep_for(50ms);
+        std::cout << "\033[2J\033[1;1H";
+        
+        std::cout << "=== БОЕВОЙ РЕЖИМ ===" << std::endl;
+        std::cout << "D - Дракон, K - Рыцарь, E - Эльф, . - мертвый" << std::endl;
+        std::cout << "Нажмите Enter для остановки" << std::endl << std::endl;
+        
+        for (int j = 0; j < grid; ++j)
+        {
+            for (int i = 0; i < grid; ++i)
+            {
+                char c = fields[i + j * grid];
+                if (c != 0)
+                    std::cout << "[" << c << "]";
+                else
+                    std::cout << "[ ]";
+            }
+            std::cout << std::endl;
+        }
+        
+        int alive_count = 0;
+        int dragon_count = 0;
+        int knight_count = 0;
+        int elf_count = 0;
+        
+        for (const auto& npc : npcs)
+        {
+            if (npc->is_alive())
+            {
+                alive_count++;
+                switch (npc->get_type())
+                {
+                case DragonType: dragon_count++; break;
+                case KnightType: knight_count++; break;
+                case ElfType: elf_count++; break;
+                }
+            }
+        }
+        
+        std::cout << std::endl;
+        std::cout << "Статистика:" << std::endl;
+        std::cout << "Живых: " << alive_count << " (Драконов: " << dragon_count 
+                  << ", Рыцарей: " << knight_count << ", Эльфов: " << elf_count << ")" << std::endl;
+        std::cout << "Мертвых: " << (npcs.size() - alive_count) << std::endl;
+        
+        std::this_thread::sleep_for(500ms);
     }
+    
+    input_thread.join();
+    
+    if (move_thread.joinable())
+        move_thread.join();
+    
+    if (fight_thread.joinable())
+        fight_thread.detach();
+    
+    std::cout << "\nБоевой режим остановлен!" << std::endl;
+    
+    set_t alive_npcs;
+    for (auto& npc : npcs)
+    {
+        if (npc->is_alive())
+            alive_npcs.insert(npc);
+    }
+    npcs = std::move(alive_npcs);
+    
+    std::cout << "Осталось живых NPC: " << npcs.size() << std::endl;
+    std::cout << "Нажмите Enter для продолжения...";
+    std::cin.get();
 }
 
-int main()
+void editor_mode(set_t& npcs)
 {
-    std::srand(static_cast<unsigned>(std::time(nullptr)));
-    
-    set_t npcs;
     bool running = true;
-    
-    std::thread collision_thread([&npcs]() {
-        collision_checker_thread(npcs, 50);
-    });
     
     while (running)
     {
@@ -228,7 +346,8 @@ int main()
         std::cout << "5. Загрузить из файла" << std::endl;
         std::cout << "6. Запустить боевой режим" << std::endl;
         std::cout << "7. Сгенерировать случайных NPC" << std::endl;
-        std::cout << "8. Выйти" << std::endl;
+        std::cout << "8. Быстрый бой" << std::endl;
+        std::cout << "9. Выйти" << std::endl;
         std::cout << "Выбор: ";
         
         int choice;
@@ -240,34 +359,40 @@ int main()
         case 1:
             add_npc_manual(npcs);
             break;
+            
         case 2:
             remove_npc(npcs);
             break;
+            
         case 3:
             print_all(npcs);
             break;
+            
         case 4:
             {
                 std::string filename;
-                std::cout << "Файл: ";
+                std::cout << "Имя файла: ";
                 std::getline(std::cin, filename);
                 save(npcs, filename);
             }
             break;
+            
         case 5:
             {
                 std::string filename;
-                std::cout << "Файл: ";
+                std::cout << "Имя файла: ";
                 std::getline(std::cin, filename);
                 npcs = load(filename);
             }
             break;
+            
         case 6:
             start_combat_mode(npcs);
             break;
+            
         case 7:
             {
-                std::cout << "Сколько: ";
+                std::cout << "Сколько NPC сгенерировать? ";
                 int count;
                 std::cin >> count;
                 clear_input();
@@ -275,22 +400,105 @@ int main()
                 for (int i = 0; i < count && i < 100; ++i)
                 {
                     NpcType type = static_cast<NpcType>((std::rand() % 3) + 1);
-                    int x = std::rand() % (MAX_X + 1);
-                    int y = std::rand() % (MAX_Y + 1);
+                    int x = std::rand() % 501;
+                    int y = std::rand() % 501;
                     auto npc = NPCFactory::create(type, x, y);
                     if (npc) npcs.insert(npc);
                 }
+                std::cout << "Сгенерировано " << std::min(count, 100) << " NPC" << std::endl;
             }
             break;
+            
         case 8:
+            {
+                std::cout << "Дальность быстрого боя: ";
+                int distance;
+                std::cin >> distance;
+                clear_input();
+                
+                if (distance > 0)
+                {
+                    set_t dead_list;
+                    
+                    for (const auto &attacker : npcs)
+                        for (const auto &defender : npcs)
+                            if ((attacker != defender) &&
+                                attacker->is_alive() &&
+                                defender->is_alive() &&
+                                attacker->is_close(defender, distance))
+                            {
+                                bool attacker_wins = defender->accept(attacker);
+                                bool defender_wins = attacker->accept(defender);
+                                
+                                if (attacker_wins && defender_wins)
+                                {
+                                    attacker->must_die();
+                                    defender->must_die();
+                                    dead_list.insert(attacker);
+                                    dead_list.insert(defender);
+                                }
+                                else if (attacker_wins)
+                                {
+                                    defender->must_die();
+                                    dead_list.insert(defender);
+                                }
+                                else if (defender_wins)
+                                {
+                                    attacker->must_die();
+                                    dead_list.insert(attacker);
+                                }
+                            }
+                    
+                    set_t alive_npcs;
+                    for (auto& npc : npcs)
+                    {
+                        if (npc->is_alive())
+                            alive_npcs.insert(npc);
+                    }
+                    npcs = std::move(alive_npcs);
+                    
+                    std::cout << "Бой завершен. Убито: " << dead_list.size() 
+                              << ", осталось: " << npcs.size() << std::endl;
+                }
+            }
+            break;
+            
+        case 9:
             running = false;
             break;
+            
+        default:
+            std::cout << "Неверный выбор!" << std::endl;
+            break;
         }
-        
-        CombatManager::get().process_events(npcs);
+    }
+}
+
+int main()
+{
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+    
+    set_t npcs;
+    
+    std::cout << "=== СИМУЛЯТОР NPC ===" << std::endl;
+    std::cout << "Правила боев:" << std::endl;
+    std::cout << "- Дракон убивает всех (включая других драконов)" << std::endl;
+    std::cout << "- Странствующий рыцарь убивает дракона" << std::endl;
+    std::cout << "- Эльф убивает странствующего рыцаря" << std::endl;
+    std::cout << "- Взаимные убийства возможны" << std::endl;
+    std::cout << "====================" << std::endl;
+    
+    std::cout << "Генерация начальных NPC..." << std::endl;
+    for (size_t i = 0; i < 10; ++i)
+    {
+        NpcType type = static_cast<NpcType>((std::rand() % 3) + 1);
+        int x = std::rand() % 501;
+        int y = std::rand() % 501;
+        auto npc = NPCFactory::create(type, x, y);
+        if (npc) npcs.insert(npc);
     }
     
-    collision_thread.detach();
+    editor_mode(npcs);
     
     return 0;
 }
---
+++ tests.cpp
@@ -0,0 +1,346 @@
+#include <gtest/gtest.h>
+#include "npc.h"
+#include "Dragon.h"
+#include "StrangeKnight.h"
+#include "Elf.h"
+#include "factory.h"
+#include "observers.h"
+#include <memory>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+class MockObserver : public IFightObserver {
+public:
+    bool fight_observed = false;
+    std::shared_ptr<NPC> last_attacker;
+    std::shared_ptr<NPC> last_defender;
+    bool last_win = false;
+    
+    void on_fight(const std::shared_ptr<NPC> attacker, 
+                  const std::shared_ptr<NPC> defender, 
+                  bool win) override {
+        fight_observed = true;
+        last_attacker = attacker;
+        last_defender = defender;
+        last_win = win;
+    }
+};
+
+TEST(NPCTest, NPCConstructor) {
+    auto dragon = make_shared<Dragon>(10, 20, "TestDragon");
+    
+    EXPECT_EQ(dragon->get_type(), DragonType);
+    EXPECT_EQ(dragon->get_name(), "TestDragon");
+    EXPECT_TRUE(dragon->is_alive());
+    
+    auto pos = dragon->position();
+    EXPECT_EQ(pos.first, 10);
+    EXPECT_EQ(pos.second, 20);
+}
+
+TEST(NPCTest, NPCSaveLoad) {
+    auto dragon1 = make_shared<Dragon>(100, 200, "DragonSaveTest");
+    
+    stringstream ss;
+    dragon1->save(ss);
+    
+    auto dragon2 = make_shared<Dragon>(ss);
+    
+    EXPECT_EQ(dragon2->get_type(), DragonType);
+    EXPECT_EQ(dragon2->get_name(), "DragonSaveTest");
+    auto pos = dragon2->position();
+    EXPECT_EQ(pos.first, 100);
+    EXPECT_EQ(pos.second, 200);
+}
+
+TEST(NPCTest, NPCMove) {
+    auto knight = make_shared<Knight>(50, 50, "MovingKnight");
+    
+    knight->move(10, -5, 100, 100);
+    auto pos = knight->position();
+    EXPECT_EQ(pos.first, 60);
+    EXPECT_EQ(pos.second, 45);
+    
+    knight->move(-70, 60, 100, 100);
+    pos = knight->position();
+    EXPECT_EQ(pos.first, 0);
+    EXPECT_EQ(pos.second, 100);
+}
+
+TEST(NPCTest, NPCIsClose) {
+    auto dragon1 = make_shared<Dragon>(0, 0, "Dragon1");
+    auto dragon2 = make_shared<Dragon>(3, 4, "Dragon2");
+    
+    EXPECT_TRUE(dragon1->is_close(dragon2, 10));
+    EXPECT_FALSE(dragon1->is_close(dragon2, 4));
+}
+
+TEST(NPCTest, NPCMustDie) {
+    auto elf = make_shared<Elf>(10, 10, "TestElf");
+    
+    EXPECT_TRUE(elf->is_alive());
+    elf->must_die();
+    EXPECT_FALSE(elf->is_alive());
+}
+
+TEST(FightTest, DragonFightRules) {
+    auto dragon = make_shared<Dragon>(0, 0, "TestDragon");
+    auto knight = make_shared<Knight>(0, 0, "TestKnight");
+    auto elf = make_shared<Elf>(0, 0, "TestElf");
+    
+    auto dragon2 = make_shared<Dragon>(0, 0, "TestDragon2");
+    
+    EXPECT_TRUE(dragon->fight(dragon2));
+    EXPECT_TRUE(dragon->fight(knight));
+    EXPECT_TRUE(dragon->fight(elf));
+}
+
+TEST(FightTest, KnightFightRules) {
+    auto dragon = make_shared<Dragon>(0, 0, "TestDragon");
+    auto knight = make_shared<Knight>(0, 0, "TestKnight");
+    auto knight2 = make_shared<Knight>(0, 0, "TestKnight2");
+    auto elf = make_shared<Elf>(0, 0, "TestElf");
+    
+    EXPECT_TRUE(knight->fight(dragon));
+    EXPECT_FALSE(knight->fight(knight2));
+    EXPECT_FALSE(knight->fight(elf));
+}
+
+TEST(FightTest, ElfFightRules) {
+    auto dragon = make_shared<Dragon>(0, 0, "TestDragon");
+    auto knight = make_shared<Knight>(0, 0, "TestKnight");
+    auto elf = make_shared<Elf>(0, 0, "TestElf");
+    auto elf2 = make_shared<Elf>(0, 0, "TestElf2");
+    
+    EXPECT_FALSE(elf->fight(dragon));
+    EXPECT_TRUE(elf->fight(knight));
+    EXPECT_FALSE(elf->fight(elf2));
+}
+
+TEST(FightTest, AcceptMethod) {
+    auto dragon = make_shared<Dragon>(0, 0, "Dragon");
+    auto knight = make_shared<Knight>(0, 0, "Knight");
+    
+    EXPECT_TRUE(knight->accept(dragon));
+    EXPECT_TRUE(dragon->accept(knight));
+}
+
+TEST(ObserverTest, FightNotification) {
+    auto observer = make_shared<MockObserver>();
+    
+    auto dragon = make_shared<Dragon>(0, 0, "Attacker");
+    auto knight = make_shared<Knight>(0, 0, "Defender");
+    
+    dragon->subscribe(observer);
+    
+    dragon->fight(knight);
+    
+    EXPECT_TRUE(observer->fight_observed);
+    EXPECT_TRUE(observer->last_win);
+    EXPECT_EQ(observer->last_attacker->get_name(), "Attacker");
+    EXPECT_EQ(observer->last_defender->get_name(), "Defender");
+}
+
+TEST(FactoryTest, CreateNPC) {
+    auto dragon = NPCFactory::create(DragonType, 10, 20, "FactoryDragon");
+    EXPECT_NE(dragon, nullptr);
+    EXPECT_EQ(dragon->get_type(), DragonType);
+    EXPECT_EQ(dragon->get_name(), "FactoryDragon");
+    
+    auto knight = NPCFactory::create(KnightType, 30, 40, "FactoryKnight");
+    EXPECT_NE(knight, nullptr);
+    EXPECT_EQ(knight->get_type(), KnightType);
+    
+    auto elf = NPCFactory::create(ElfType, 50, 60, "FactoryElf");
+    EXPECT_NE(elf, nullptr);
+    EXPECT_EQ(elf->get_type(), ElfType);
+}
+
+TEST(FactoryTest, LoadNPC) {
+    stringstream ss;
+    ss << DragonType << endl;
+    ss << 100 << endl;
+    ss << 200 << endl;
+    ss << "StreamDragon" << endl;
+    
+    auto npc = NPCFactory::load(ss);
+    EXPECT_NE(npc, nullptr);
+    EXPECT_EQ(npc->get_type(), DragonType);
+    EXPECT_EQ(npc->get_name(), "StreamDragon");
+}
+
+TEST(FactoryTest, SaveLoadAll) {
+    set_t npcs;
+    
+    npcs.insert(NPCFactory::create(DragonType, 1, 2, "D1"));
+    npcs.insert(NPCFactory::create(KnightType, 3, 4, "K1"));
+    npcs.insert(NPCFactory::create(ElfType, 5, 6, "E1"));
+    
+    save(npcs, "test_save.txt");
+    
+    auto loaded = load("test_save.txt");
+    EXPECT_EQ(loaded.size(), 3);
+    
+    remove("test_save.txt");
+}
+
+TEST(CombatTest, CombatResolution) {
+    auto dragon = make_shared<Dragon>(0, 0, "Dragon");
+    auto knight = make_shared<Knight>(0, 0, "Knight");
+    auto elf = make_shared<Elf>(0, 0, "Elf");
+    
+    EXPECT_TRUE(knight->accept(dragon));
+    EXPECT_FALSE(dragon->accept(knight));
+    
+    EXPECT_TRUE(elf->accept(knight));
+    EXPECT_FALSE(knight->accept(elf));
+    
+    EXPECT_TRUE(dragon->accept(elf));
+    EXPECT_FALSE(elf->accept(dragon));
+}
+
+TEST(TypeTest, TypeStrings) {
+    auto dragon = make_shared<Dragon>(0, 0, "");
+    auto knight = make_shared<Knight>(0, 0, "");
+    auto elf = make_shared<Elf>(0, 0, "");
+    
+    EXPECT_EQ(dragon->get_type_str(), "Дракон");
+    EXPECT_EQ(knight->get_type_str(), "Странствующий рыцарь");
+    EXPECT_EQ(elf->get_type_str(), "Эльф");
+}
+
+TEST(SetTest, NPCOperator) {
+    auto dragon = make_shared<Dragon>(10, 20, "TestOp");
+    
+    stringstream ss;
+    ss << *dragon;
+    
+    string output = ss.str();
+    EXPECT_NE(output.find("Дракон"), string::npos);
+    EXPECT_NE(output.find("TestOp"), string::npos);
+    EXPECT_NE(output.find("x:10"), string::npos);
+    EXPECT_NE(output.find("y:20"), string::npos);
+}
+
+TEST(SetTest, PrintAll) {
+    set_t npcs;
+    npcs.insert(NPCFactory::create(DragonType, 1, 1, "D1"));
+    npcs.insert(NPCFactory::create(KnightType, 2, 2, "K1"));
+    
+    EXPECT_NO_THROW(print_all(npcs));
+}
+
+TEST(CombatTest, MutualKill) {
+    auto dragon1 = make_shared<Dragon>(0, 0, "Dragon1");
+    auto dragon2 = make_shared<Dragon>(0, 0, "Dragon2");
+    
+    EXPECT_TRUE(dragon1->fight(dragon2));
+    EXPECT_TRUE(dragon2->fight(dragon1));
+}
+
+TEST(FileObserverTest, FileCreation) {
+    auto file_observer = FileObserver::get();
+    
+    auto dragon = make_shared<Dragon>(0, 0, "FileTestDragon");
+    auto knight = make_shared<Knight>(0, 0, "FileTestKnight");
+    
+    dragon->subscribe(file_observer);
+    
+    dragon->fight(knight);
+    
+    ifstream log_file("log.txt");
+    EXPECT_TRUE(log_file.good());
+    
+    if (log_file.is_open()) {
+        string content((istreambuf_iterator<char>(log_file)), 
+                       istreambuf_iterator<char>());
+        EXPECT_FALSE(content.empty());
+        log_file.close();
+    }
+    
+    remove("log.txt");
+}
+
+TEST(TextObserverTest, Singleton) {
+    auto observer1 = TextObserver::get();
+    auto observer2 = TextObserver::get();
+    
+    EXPECT_EQ(observer1.get(), observer2.get());
+}
+
+TEST(NPCTest, AutoNameGeneration) {
+    auto dragon1 = make_shared<Dragon>(0, 0, "");
+    auto dragon2 = make_shared<Dragon>(0, 0, "");
+    
+    EXPECT_NE(dragon1->get_name(), dragon2->get_name());
+    EXPECT_EQ(dragon1->get_name().substr(0, 4), "NPC_");
+    EXPECT_EQ(dragon2->get_name().substr(0, 4), "NPC_");
+}
+
+TEST(NPCTest, SetName) {
+    auto dragon = make_shared<Dragon>(0, 0, "Original");
+    
+    EXPECT_EQ(dragon->get_name(), "Original");
+    
+    dragon->set_name("Changed");
+    EXPECT_EQ(dragon->get_name(), "Changed");
+}
+
+TEST(FightTest, DeadNPCNoFight) {
+    auto dragon = make_shared<Dragon>(0, 0, "Dragon");
+    auto knight = make_shared<Knight>(0, 0, "Knight");
+    
+    knight->must_die();
+    
+    EXPECT_FALSE(knight->is_alive());
+}
+
+TEST(FactoryTest, InvalidType) {
+    auto npc = NPCFactory::create(static_cast<NpcType>(999), 0, 0, "Invalid");
+    EXPECT_EQ(npc, nullptr);
+}
+
+TEST(NPCTest, SharedFromThis) {
+    auto dragon = make_shared<Dragon>(0, 0, "Test");
+    
+    EXPECT_NO_THROW({
+        auto self = dragon->shared_from_this();
+        EXPECT_EQ(self.get(), dragon.get());
+    });
+}
+
+TEST(ObserverTest, MultipleObservers) {
+    auto observer1 = make_shared<MockObserver>();
+    auto observer2 = make_shared<MockObserver>();
+    
+    auto dragon = make_shared<Dragon>(0, 0, "Dragon");
+    auto knight = make_shared<Knight>(0, 0, "Knight");
+    
+    dragon->subscribe(observer1);
+    dragon->subscribe(observer2);
+    
+    dragon->fight(knight);
+    
+    EXPECT_TRUE(observer1->fight_observed);
+    EXPECT_TRUE(observer2->fight_observed);
+}
+
+TEST(CombatTest, NoFightWhenDead) {
+    auto dragon = make_shared<Dragon>(0, 0, "Dragon");
+    auto knight = make_shared<Knight>(0, 0, "Knight");
+    
+    knight->must_die();
+    
+    auto observer = make_shared<MockObserver>();
+    dragon->subscribe(observer);
+    
+    EXPECT_FALSE(knight->is_alive());
+    EXPECT_FALSE(dragon->fight(knight));
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}

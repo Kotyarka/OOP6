15336c7d957c1010bc7609df94f3527d89450d3a
Костян <varenik.kotyarka@gmail.com>
2025-12-03 23:42:17 +0300

    Created files

---
+++ npc.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <cstring>
+#include <string>
+#include <random>
+#include <fstream>
+#include <set>
+#include <math.h>
+#include <shared_mutex>
